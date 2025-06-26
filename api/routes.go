1ac10caec6b5941a3727d709276f8ecbe867ddc3
kimtree <iam@kimtree.net>
2025-06-26 17:59:11 +0900

    Apply v1 routes

--- api/routes.go
+++ api/routes.go
@@ -6,10 +6,14 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	r.POST("/users", user.CreateHandler)
-	r.GET("/users", user.GetAllHandler)
-	r.GET("/users/:id", user.GetByIDHandler)
-	r.PUT("/users/:id", user.UpdateAllHandler)
-	r.PATCH("/users/:id", user.UpdatePartialHandler)
-	r.DELETE("/users/:id", user.DeleteHandler)
+	{
+		v1 := r.Group("/v1")
+
+		v1.POST("/users", user.CreateHandler)
+		v1.GET("/users", user.GetAllHandler)
+		v1.GET("/users/:id", user.GetByIDHandler)
+		v1.PUT("/users/:id", user.UpdateAllHandler)
+		v1.PATCH("/users/:id", user.UpdatePartialHandler)
+		v1.DELETE("/users/:id", user.DeleteHandler)
+	}
 }
